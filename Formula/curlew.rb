b194782fb62c4835c97c77b763121a5fc5e9fe2b
Joel Pan <joelpan@amazon.com>
2026-06-15 13:46:10 -0700

    feat: add curlew formula v0.1.0

---
+++ Formula/curlew.rb
@@ -0,0 +1,16 @@
+class Curlew < Formula
+  desc "Inspect before you execute. A safe wrapper for curl|bash"
+  homepage "https://github.com/ketsugi/curlew"
+  url "https://github.com/ketsugi/curlew/releases/download/v0.1.0/curlew"
+  sha256 "b969e0bb2133a89f16e0698bab7a4b3f7e8fe2dd039b2778e6a41ac3885fd378"
+  license "MIT"
+  version "0.1.0"
+
+  def install
+    bin.install "curlew"
+  end
+
+  test do
+    assert_match "curlew #{version}", shell_output("#{bin}/curlew --version")
+  end
+end
